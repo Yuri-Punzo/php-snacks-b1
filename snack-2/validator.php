5f56b600e6a0b7ed9fe3b9d698373212defbb948
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-28 13:40:37 +0100

    snack 2 final commit

--- snack-2/validator.php
+++ snack-2/validator.php
@@ -5,6 +5,17 @@ $email = $_GET["Email"];
 
 $age = $_GET["Age"];
 
-echo $name . "<br>";
+$acces = "";
+
+/* echo $name . "<br>";
 echo $email . "<br>";
-echo $age . "<br>";
+echo $age . "<br>"; */
+
+
+if (strlen($name) > 3 && str_contains($email, ".") && str_contains($email, "@") && is_numeric($age)) {
+    $access = 'Accesso riuscito';
+    echo $access;
+} else {
+    $access =  'Accesso negato';
+    echo $access;
+};
